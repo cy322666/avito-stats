4057dfaa44841a879ed35ba3c75896e5f1e47dca
cy322666 <cy322666@gmail.com>
2022-07-05 18:29:23 +0200

    change schedulers time

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -15,12 +15,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('avito:ads')->everyTenMinutes();
-         $schedule->command('avito:ads-stats')->everyTenMinutes();
-         $schedule->command('avito:ads-calls')->everyTenMinutes();
-         $schedule->command('avito:ads-service')->everyTenMinutes();
+         $schedule->command('avito:ads')->everyMinute();
+         $schedule->command('avito:ads-stats')->everyMinute();
+         $schedule->command('avito:ads-calls')->everyMinute();
+         $schedule->command('avito:ads-service')->everyMinute();
 
-         $schedule->command('sipout:calls')->everyTenMinutes();
+         $schedule->command('sipout:calls')->everyMinute();
     }
 
     /**
